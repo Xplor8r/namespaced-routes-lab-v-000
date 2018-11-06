2a4d54257d8c555ce500dcec5e9ed9a8af523358
Michael Pieschel <sixtyhectares@yahoo.com>
2018-11-06 22:09:56 +0000

    Done.

---
+++ app/helpers/preferences_helper.rb
@@ -0,0 +1,14 @@
+module PreferencesHelper
+
+  def allow_create_artists
+
+  end
+
+  def allow_create_songs
+
+  end
+
+  def artist_sort_order
+
+  end
+end
--- db/schema.rb
+++ db/schema.rb
@@ -11,10 +11,20 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20160126235547) do
+ActiveRecord::Schema.define(version: 20181106214149) do
+
+  create_table "admins", force: :cascade do |t|
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+  end
 
   create_table "artists", force: :cascade do |t|
     t.string   "name"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+  end
+
+  create_table "preferences", force: :cascade do |t|
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
   end
